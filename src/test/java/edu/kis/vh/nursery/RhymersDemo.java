996239edb407ff5feeaa57cabeca713f05e46635
Piotr Marczak <251184@edu.p.lodz.pl>
2025-04-02 12:16:10 +0200

    Dodano javadoc, szkielet metod

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -5,37 +5,47 @@ import edu.kis.vh.nursery.HanoiRhymer;
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
 import edu.kis.vh.nursery.factory.Rhymersfactory;
 
+/**
+ * Klasa demonstrująca działanie różnych typów stosów (rhymerów).
+ * Program tworzy różne typy stosów przy użyciu fabryki, zapełnia je liczbami i następnie wypisuje zawartość każdego z nich.
+ * Program również wyświetla liczbę odrzuconych elementów dla typu HanoiRhymer.
+ */
 class RhymersDemo {
 
+    /**
+     * Główna metoda programu, która tworzy instancję fabryki rhymerów i wywołuje metodę testującą.
+     * Metoda testuje działanie różnych typów rhymerów poprzez zapełnienie ich liczbami
+     * i wypisanie elementów z poszczególnych stosów.
+     */
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
-        testRhymers(factory);  // Wywołanie nowej metody
+        testRhymers(factory);
     }
 
-    // Nowa metoda przyjmująca fabrykę stosów
+    /**
+     * Testuje działanie różnych typów rhymerów.
+     * Tworzy tablicę rhymerów, zapełnia ją liczbami i losowymi liczbami, a następnie wypisuje elementy z każdego stosu.
+     * Dodatkowo, dla typu HanoiRhymer, wypisuje liczbę odrzuconych elementów.
+     */
     public static void testRhymers(Rhymersfactory factory) {
-        // Tworzenie tablicy rhymerów przy użyciu fabryki
         defaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
                 factory.GetFIFORhymer(), factory.GetHanoiRhymer() };
-
-        // Zapełnianie 3 pierwszych stosów liczbami
         for (int i = 1; i < 15; i++)
             for (int j = 0; j < 3; j++)
                 rhymers[j].countIn(i);
 
-        // Zapełnianie 4. stosu losowymi liczbami
+
         java.util.Random rn = new java.util.Random();
         for (int i = 1; i < 15; i++)
             rhymers[3].countIn(rn.nextInt(20));
 
-        // Wypisywanie elementów z każdego stosu
+
         for (int i = 0; i < rhymers.length; i++) {
             while (!rhymers[i].callCheck())
                 System.out.print(rhymers[i].countOut() + "  ");
             System.out.println();
         }
 
-        // Wypisywanie liczby odrzuconych elementów w HanoiRhymer
         System.out.println("total rejected is "
                 + ((HanoiRhymer) rhymers[3]).reportRejected());
     }
