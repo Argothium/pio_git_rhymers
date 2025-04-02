4208b3fbea82871f0db514bc85affb5ce1146ab4
Piotr Marczak <251184@edu.p.lodz.pl>
2025-04-02 12:01:01 +0200

    Uporzdkowanie aplikacji demo

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -9,27 +9,34 @@ class RhymersDemo {
 
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
-        
+        testRhymers(factory);  // Wywołanie nowej metody
+    }
+
+    // Nowa metoda przyjmująca fabrykę stosów
+    public static void testRhymers(Rhymersfactory factory) {
+        // Tworzenie tablicy rhymerów przy użyciu fabryki
         defaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
-                factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
-        
+                factory.GetFIFORhymer(), factory.GetHanoiRhymer() };
+
+        // Zapełnianie 3 pierwszych stosów liczbami
         for (int i = 1; i < 15; i++)
             for (int j = 0; j < 3; j++)
                 rhymers[j].countIn(i);
-        
+
+        // Zapełnianie 4. stosu losowymi liczbami
         java.util.Random rn = new java.util.Random();
         for (int i = 1; i < 15; i++)
             rhymers[3].countIn(rn.nextInt(20));
-        
+
+        // Wypisywanie elementów z każdego stosu
         for (int i = 0; i < rhymers.length; i++) {
             while (!rhymers[i].callCheck())
                 System.out.print(rhymers[i].countOut() + "  ");
             System.out.println();
         }
-        
+
+        // Wypisywanie liczby odrzuconych elementów w HanoiRhymer
         System.out.println("total rejected is "
                 + ((HanoiRhymer) rhymers[3]).reportRejected());
-        
     }
-    
 }
